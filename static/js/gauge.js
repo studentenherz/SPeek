e6e3d81a91168395ec75f235307e1d60deeeece2
Michel Romero <studentenherz@gmail.com>
2022-01-27 12:32:32 -0300

    also change text

--- static/js/gauge.js
+++ static/js/gauge.js
@@ -1,4 +1,7 @@
 let setGaugeValue = (id, value) => {
 	let gauge = document.querySelector(`#${id} .gauge`);
 	gauge.style.background = `conic-gradient(#11921c ${value}%, #acacac ${value}%)`;
+
+	let text = document.querySelector(`#${id} .gauge__text`);
+	text.textContent = `${value}%`
 }
