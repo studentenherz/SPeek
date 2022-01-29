75a26689815e7a1a31a607531ca7d3c4b008276d
Michel Romero <studentenherz@gmail.com>
2022-01-29 16:20:05 -0300

    flash also when password incorrect

--- speek/auth.py
+++ speek/auth.py
@@ -19,8 +19,7 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		user = User.query.filter_by(username=form.username.data).first()
-		if user:
-			if bcrypt.check_password_hash( user.password, form.password.data):
+		if user and bcrypt.check_password_hash( user.password, form.password.data):
 				login_user(user)
 				return redirect(url_for('main.dashboard'))
 		else:
