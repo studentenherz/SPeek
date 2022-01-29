d7bb461506350503a2ef1ce52538fa905b034d83
Michel Romero <studentenherz@gmail.com>
2022-01-29 16:09:05 -0300

    gunicorn wsgi runnable

--- speek/__init__.py
+++ speek/__init__.py
@@ -1,3 +1,7 @@
+'''
+	Run the application with gunicorn speek:app
+'''
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -8,29 +12,25 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
 
-def create_app():
-	app = Flask(__name__)
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-	app.config['SECRET_KEY'] = os.getenv('SPEEK_KEY', 'default_secret_key_not_very_secret')
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = os.getenv('SPEEK_KEY', 'default_secret_key_not_very_secret')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-	db.init_app(app)
-	bcrypt.init_app(app)
-	
-	login_manager.init_app(app)
-	login_manager.login_view = 'auth.login'
+db.init_app(app)
+bcrypt.init_app(app)
+
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
 
 
-	from .main import main as main_blueprint
-	app.register_blueprint(main_blueprint)
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
 
-	from .auth import auth as auth_blueprint
-	app.register_blueprint(auth_blueprint)
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-	with app.app_context():
-		db.create_all()
-		from .auth import register_admin
-		register_admin()
-
-	return app
-	
+with app.app_context():
+	db.create_all()
+	from .auth import register_admin
+	register_admin()	
