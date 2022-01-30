fa6461e25c9ebfd847ba3c1ffc01f30f31f86032
Michel Romero <studentenherz@gmail.com>
2022-01-30 03:06:56 -0300

    logo and dynamic favicons

--- speek/static/js/main.js
+++ speek/static/js/main.js
@@ -15,11 +15,21 @@ main = () => {
 		if (value > 50) level = 'warning';
 		if (value > 90) level = 'error';
 
-		document.querySelector(`#${id} .gauge`).classList.remove('ok', 'warning', 'error')
-		document.querySelector(`#${id} .gauge`).classList.add(level)
+		document.querySelector(`#${id} .gauge`).classList.remove('ok', 'warning', 'error');
+		document.querySelector(`#${id} .gauge`).classList.add(level);
 
-		return level
+		return level;
 	}
+
+	let change_favicon = (level) => {
+		let link = document.querySelector("link[rel~='icon']");
+		if (!link) {
+			let link = document.createElement('link');
+			link.rel = 'icon';
+			document.getElementsByTagName('head')[0].appendChild(link);
+		}
+		link.href = `/static/img/favicon-${level}.svg`;
+	};
 
 	updateStatus = () => {
 		fetch('/getStatus')
@@ -29,27 +39,28 @@ main = () => {
 
 				// Load
 				setGaugeValue('load', data['load']['percent']);
-				load_level = setLevel('load', data['load']['percent'])
-				document.querySelector('#load .extra').textContent = load_status_message[load_level]
+				load_level = setLevel('load', data['load']['percent']);
+				document.querySelector('#load .extra').textContent = load_status_message[load_level];
+				change_favicon(load_level);
 
 				// CPU
 				setGaugeValue('cpu', data['cpu']['percent']);
-				setLevel('cpu', data['cpu']['percent'])
+				setLevel('cpu', data['cpu']['percent']);
 
 				// RAM
 				setGaugeValue('ram', data['ram']['percent']);
-				setLevel('ram', data['ram']['percent'])
-				document.querySelector('#ram .extra span').textContent = parseFloat(data['ram']['used']).toFixed(1)
+				setLevel('ram', data['ram']['percent']);
+				document.querySelector('#ram .extra span').textContent = parseFloat(data['ram']['used']).toFixed(1);
 
 				// Swap
 				setGaugeValue('swap', data['swap']['percent']);
 				setLevel('swap', data['swap']['percent']);
-				document.querySelector('#swap .extra span').textContent = parseFloat(data['swap']['used']).toFixed(1)
+				document.querySelector('#swap .extra span').textContent = parseFloat(data['swap']['used']).toFixed(1);
 
 				// Disk usage '/'
 				setGaugeValue('disk-root', data['disk']['percent']);
 				setLevel('disk-root', data['disk']['percent']);
-				document.querySelector('#disk-root .extra span').textContent = parseFloat(data['disk']['used']).toFixed(1)
+				document.querySelector('#disk-root .extra span').textContent = parseFloat(data['disk']['used']).toFixed(1);
 			});
 	};
 
